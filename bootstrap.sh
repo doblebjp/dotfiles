ac4e47ec17e3b953856ecde7ab90bc3401b4b5d4
Benedict <benedict@hoscobene.local>
2018-05-04 10:50:05 +0200

    Remove YouCompleteMe compilation

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,9 +13,6 @@ npm install -g tslint
 
 composer global require "squizlabs/php_codesniffer"
 
-# compile YouCompleteMe
-# (cd .vim/bundle/YouCompleteMe && ./install.py --clang-completer --tern-completer)
-
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
